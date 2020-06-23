62f39ec3206dc1d012ce462c8ccc3b7bb360cbe8
Ansh <Ansh@Ansh>
2020-06-22 22:06:11 -0400

    javascript started

--- public_html/TicTacToeA2.js
+++ public_html/TicTacToeA2.js
@@ -1,7 +1,26 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* Ansh */
+
+// TODO document this
+var board = document.getElementsByTagName("td");
+
+// assuming we index the 9 tic tac toe cells from left to right, top to
+// bottom, as 0-8, these would be all of the winning combinations:
+var nextTurn;
+var winSets = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+// X always gets to go first
+var player = "X";
+
+// keep track of how many cells are empty at any time
+var empty = 9;
+
+// keep track of game status - false if still playing
+var gameOver = false;
+
+/* Function resetGame() is called when user clicks on the "game reset" button
+ 1. sets content of all 9 cells to nothing
+ 2. sets the starting player (this version, X always starts the game)
+ 3. updates the message to the current player
+ 4. resets the number of empty cells to 9
+ 5. sets the game over flag to false to indicate that the game is in progress
  */
-
-
